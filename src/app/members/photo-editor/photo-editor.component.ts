a131a59f04d1469eb61d6c8dbb3304face4404a4
nikhil <nikhil.m@imimobile.com>
2020-09-03 21:18:52 +0530

    session11

--- src/app/members/photo-editor/photo-editor.component.ts
+++ src/app/members/photo-editor/photo-editor.component.ts
@@ -74,4 +74,16 @@ export class PhotoEditorComponent implements OnInit {
     });
   }
 
+  deletePhoto(id: number)
+  {
+    this.alertify.confirm('Are you sure want to delete this photo?', () => {
+      this.userService.deletePhoto(this.authService.decodedToken.nameid,id).subscribe(() => {
+        this.photos.splice(this.photos.findIndex(p => p.id === id), 1);
+        this.alertify.success('photo has been deleted');
+      }, error => {
+        this.alertify.error('failed to delete the photo');
+      });
+    });
+  }
+
 }
